8367f4bef6dedf4450a29dc35fc6e67fd1d9628b
raspberryenvoie <raspberryenvoie@protonmail.com>
2026-01-21 15:42:44 +0000

    Fix postscript

--- post-scripts/combine_mpas.sh
+++ post-scripts/combine_mpas.sh
@@ -2,7 +2,27 @@
 DEST_DIR="$(dirname $0)/combined_mpas_$(date +%s)" # Add Unix epoch to make unique
 ENV_PATH="$(dirname $0)/.conda/krakentools"
 
-source "$(dirname $0)/.common.sh"
+set -euo pipefail
+if [ "$#" -eq 0 ]; then
+    echo "Usage: $0 <directory1> <directory2> ..."
+    exit 1
+fi
+
+for dir in "$@"; do
+    if [ ! -d "$dir" ]; then
+        echo "$dir is not a valid directory. Exiting."
+        exit 1
+    fi
+done
+
+mkdir -p "$DEST_DIR"
+
+for dir in "$@"; do
+    echo "Copying contents of directory: $dir to $DEST_DIR"
+    cp -r "$dir/"*.mpa "$DEST_DIR/"
+done
+
+echo "All contents copied successfully."
 
 if [ ! -d "$ENV_PATH" ]; then
     echo "Creating new environment at: $ENV_PATH"
@@ -15,7 +35,7 @@ echo 'Combining reports'
 conda run -p "$ENV_PATH" bash -c "
     set -euo pipefail
     cd "$DEST_DIR"
-    combine_mpa.py -i ./*.mpa -o 'combined.kreports.mpa'
+    combine_mpa.py -i ./*.mpa -o 'all_combined_report.kreports.mpa'
 "
 
 echo "Done!"
